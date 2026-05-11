935594d29b3a00ba4b90251615fe2819d05542d9
Reinaldo Gramacho <reinaldogramachof@gmail.com>
2026-05-11 20:28:11 -0300

    refactor: adicionar componente Divider

---
+++ src/components/ui/Divider.tsx
@@ -0,0 +1,11 @@
+import { colors } from '../../styles/theme';
+
+export const Divider = () => (
+  <div
+    style={{
+      height: 1,
+      background: colors.border,
+      margin: '12px 0',
+    }}
+  />
+);
