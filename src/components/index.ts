f9b2790f5f19a98fe2cc8496b9d71f400903b7ca
Reinaldo Gramacho <reinaldogramachof@gmail.com>
2026-05-11 20:28:34 -0300

    refactor: adicionar barrel de componentes

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Avatar } from './Avatar';
+export { Logo } from './Logo';
+export { Badge } from './ui/Badge';
+export { Divider } from './ui/Divider';
+export { ProgressRing } from './ui/ProgressRing';
